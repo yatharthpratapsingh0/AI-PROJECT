a8c2fbb71c8ef4349723985ea5e237c6d155840a
Yatharth Pratap Singh <sgs.yatharth.febuary.7@gmail.com>
2026-01-17 18:10:05 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "stealth": "https://www.youtube.com/watch?v=U47Tr9BB_wE",
+    "march": "https://www.youtube.com/watch?v=Xqeq4b5u_Xw",
+    "skyfall": "https://www.youtube.com/watch?v=DeumyOzKqgI&pp=ygUHc2t5ZmFsbA%3D%3D",
+    "Ordinary (Official Video)": "https://youtu.be/u2ah9tWTkmk?list=RDu2ah9tWTkmk"
+}
